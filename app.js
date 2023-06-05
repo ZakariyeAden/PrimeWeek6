76117341b0024627ca136565517077124de08440
ZakariyeAden <zakariye5417@gmail.com>
2023-06-05 11:18:01 -0500

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+// Test
+console.log('Hello');
+
+// **********************
+// Values and Expressions
+// **********************
+
+// 1.) Declare a Variable 
+let myBirthday;
+
+console.log(`My Birthday is: ${myBirthday}`);
+// Assign a value to variable
+myBirthday = '01/31/2000';
+
+// Declare a variable and assign it on the same line
+let myBrotherBirthday = '01/01/1990';
