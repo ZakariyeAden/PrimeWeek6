a816c9ab73cae5793410488f074ac875ba80f138
ZakariyeAden <zakariye5417@gmail.com>
2023-06-05 14:41:15 -0500

    update

--- Script/script.js
+++ Script/script.js
@@ -24,4 +24,31 @@ let animals = [
     name: 'Fish',
     awesomeLevel: 4,
   },
-]
+];
+
+
+/* 
+  Lets write some psuedo code!
+
+  * Make a function that takes in a box of items as an argue
+  * Declare a new variable
+  * Assign it to value to an empty array
+* Loop through the box of items
+  * IF
+    * Check item awesomeLevel is > 7
+    * Push the item into the new array
+  * ELSE
+    * Do nothing i guess
+* Return the new Array 
+  
+
+*/
+// Input:
+    // Array of Objects, where each object
+    // has a name and awesomeLevel property
+// Output:
+    // An array of awesomeLevel where each object 
+    // awesomeLevel property is above 7
+function findAwesomeItem(item){
+
+}
